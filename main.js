2120511806ffb355d96d3c54687b9023c922159d
Michael Rezq <michaelrezq9@gmail.com>
2023-01-28 12:56:51 +0200

    main components

---
+++ main.js
@@ -0,0 +1,31 @@
+// slider in home page
+let slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+function changeImg() {
+  myInterval = setInterval(function () {
+    showSlides(slideIndex);
+    slideIndex = slideIndex + 1;
+  }, 2000);
+}
+window.onload = changeImg;
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
